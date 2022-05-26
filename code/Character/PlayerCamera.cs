4c22c41812406a841d5dbc29199c53559b53c5b0
Tony Ferguson <devultj@gmail.com>
2022-05-27 00:02:34 +0100

    Set up proper camera features

--- code/Character/PlayerCamera.cs
+++ code/Character/PlayerCamera.cs
@@ -1,26 +1,58 @@
+
 namespace Rising;
 
 public partial class PlayerCamera : CameraMode
 {
-	public BaseCharacter Character => Entity as BaseCharacter;
+	protected Angles OrbitAngles;
 
-	protected float UpAmount => 128f;
-	protected float BackAmount => 64f;
+	protected float OrbitDistance { get; set; } = 400f;
+	protected float WheelSpeed => 10f;
 
 	public override void Update()
 	{
-		if ( !Character.IsValid() )
+		var pawn = Local.Pawn as AnimatedEntity;
+
+		if ( pawn == null )
 			return;
 
-		Position = Character.Position + Vector3.Up * UpAmount + Character.Rotation.Backward * BackAmount;
-		Rotation = Character.Rotation * Rotation.FromPitch( 45 );
+		Position = pawn.Position;
+		Vector3 targetPos;
+
+		Position += Vector3.Up * (pawn.CollisionBounds.Center.z * pawn.Scale);
+		Rotation = Rotation.From( OrbitAngles );
+
+		targetPos = Position + Rotation.Backward * OrbitDistance;
+
+		Position = targetPos;
+
+		FieldOfView = 70;
+
+		Viewer = null;
 	}
 
-	public override void Build( ref CameraSetup camSetup )
+	public override void BuildInput( InputBuilder input )
 	{
-		base.Build( ref camSetup );
+		if ( input.Down( InputButton.SecondaryAttack ) )
+		{
+			var wheel = input.MouseWheel;
 
-		camSetup.Position = Position;
-		camSetup.Rotation = Rotation;
+			if ( wheel != 0 )
+			{
+				OrbitDistance -= wheel * WheelSpeed;
+				OrbitDistance = OrbitDistance.Clamp( 300, 500 );
+			}
+			else
+			{
+				OrbitAngles.yaw += input.AnalogLook.yaw;
+				OrbitAngles.pitch += input.AnalogLook.pitch;
+				OrbitAngles = OrbitAngles.Normal;
+				OrbitAngles.pitch = OrbitAngles.pitch.Clamp( 50, 80 );
+			}
+
+			input.AnalogLook = Angles.Zero;
+		}
+
+		// Let players move around at will
+		input.InputDirection = input.AnalogMove;
 	}
 }
