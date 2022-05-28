ed2352c596211070f371442108f41083819c511e
Tony Ferguson <devultj@gmail.com>
2022-05-28 03:07:07 +0100

    HoldType, HoldHandedness

--- code/Weapons/Base/BaseCarriable.cs
+++ code/Weapons/Base/BaseCarriable.cs
@@ -3,6 +3,8 @@ namespace Spire;
 public partial class BaseCarriable : Sandbox.BaseCarriable
 {
 	public virtual string ModelPath => "";
+	public virtual HoldType HoldType => HoldType.None;
+	public virtual HoldHandedness HoldHandedness => HoldHandedness.TwoHands;
 
 	public override void Spawn()
 	{
@@ -12,5 +14,14 @@ public partial class BaseCarriable : Sandbox.BaseCarriable
 		{
 			SetModel( ModelPath );
 		}
+	}
+
+
+	public override void SimulateAnimator( PawnAnimator anim )
+	{
+		base.SimulateAnimator( anim );
+
+		anim.SetAnimParameter( "holdtype", (int)HoldType );
+		anim.SetAnimParameter( "holdtype_handedness", (int)HoldHandedness );
 	}
 }
