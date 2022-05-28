f04738cb6367723dc8e0a0a621ff775b20e73a63
Tony Ferguson <devultj@gmail.com>
2022-05-28 03:07:34 +0100

    set up SwordWeapon in prep for asset

--- code/Weapons/MeleeWeapons/SwordWeapon.cs
+++ code/Weapons/MeleeWeapons/SwordWeapon.cs
@@ -2,4 +2,13 @@ namespace Spire;
 
 public partial class SwordWeapon : BaseMeleeWeapon
 {
+	public override HoldType HoldType => HoldType.Item;
+	public override HoldHandedness HoldHandedness => HoldHandedness.RightHand;
+
+	public override void SimulateAnimator( PawnAnimator anim )
+	{
+		base.SimulateAnimator( anim );
+
+		anim.SetAnimParameter( "holdtype_pose_hand", 0.07f );
+	}
 }
