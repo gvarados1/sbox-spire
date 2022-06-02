7c4ab15f4c67d0250efa5962b1afb160eb412a02
Tony Ferguson <devultj@gmail.com>
2022-06-02 13:59:05 +0100

    Use Projectile for InstantDamageAbility

--- code/Character/Abilities/InstantDamageAbility.cs
+++ code/Character/Abilities/InstantDamageAbility.cs
@@ -8,12 +8,49 @@ public partial class InstantDamageAbility : PlayerAbility
 	public override string AbilityIcon => "ui/ability_icons/bomb.png";
 	public override PlayerAbilityType Type => PlayerAbilityType.Standard;
 
+	public virtual float ProjectileSpeed => 500f;
+	public virtual float ProjectileRadius => 10f;
+	public virtual float ProjectileThrowStrength => 100f;
+
 	public override void Execute()
 	{
 		base.Execute();
 
-		Log.Info( "Instant Damage!" );
+		if ( Host.IsClient )
+			return;
 
-		Character.TakeDamage( DamageInfo.Generic( 30f ).WithPosition( Character.Position ) );
+		var projectile = new BouncyProjectileEntity()
+		{
+			Bounciness = 0.7f,
+			IgnoreEntity = Character,
+			Attacker = Character,
+			LifeTime = 2.5f,
+			Gravity = 15f,
+			ModelPath = "models/sbox_props/watermelon/watermelon.vmdl"
+		};
+
+		var position = Character.EyePosition + Vector3.Down * 25f;
+		var forward = Character.EyeRotation.Forward;
+		var endPosition = position + Vector3.Up * 256f + forward * 200f;
+		var trace = Trace.Ray( position, endPosition )
+			.Ignore( Character )
+			.Run();
+
+		var direction = (trace.EndPosition - position).Normal;
+		direction = direction.Normal;
+
+		var velocity = (direction * ProjectileSpeed) + (Character.EyeRotation.Forward * ProjectileThrowStrength);
+		projectile.Initialize( position, velocity, ProjectileRadius, OnProjectileHit );
+	}
+
+	protected void OnProjectileHit( ProjectileEntity projectile, Entity hitEntity )
+	{
+		new ExplosionEntity
+		{
+			Position = projectile.Position,
+			Radius = 256f,
+			Damage = 50f,
+			ForceScale = 1f,
+		}.Explode( projectile );
 	}
 }
