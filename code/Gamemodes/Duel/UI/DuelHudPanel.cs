1d29b09baf26c2e4746d6bd0924d74c8bf21471b
Tony <devultj@gmail.com>
2022-06-09 14:58:54 +0100

    Show score info on screen

--- code/Gamemodes/Duel/UI/DuelHudPanel.cs
+++ code/Gamemodes/Duel/UI/DuelHudPanel.cs
@@ -6,4 +6,7 @@ public partial class DuelHudPanel : Panel
 	public DuelGamemode Gamemode => BaseGamemode.Current as DuelGamemode;
 
 	public string GameState => Gamemode?.CurrentState.GetName();
+
+	public string RedScore => $"{Gamemode?.GetTeamScore( DuelTeam.Red )}";
+	public string BlueScore => $"{Gamemode?.GetTeamScore( DuelTeam.Blue )}";
 }
