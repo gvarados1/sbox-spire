6b3215e2fd7c562cf6160f082a0bfb49f318f84b
Tony <devultj@gmail.com>
2022-06-11 21:54:52 +0100

    Post-merge fix

--- code/Abilities/WeaponAbilities/SwordSlash.cs
+++ code/Abilities/WeaponAbilities/SwordSlash.cs
@@ -5,7 +5,4 @@ public partial class SwordAttack : BaseMeleeAttackAbility
 	// Configuration
 	public override string Identifier => "sword_attack";
 	public override WeaponAbilityType Type => WeaponAbilityType.Attack;
-	public override string PreAbilityParticle => "particles/weapons/basic_sword/basic_swipe/basic_swipe.vpcf";
-	public override bool OriginateParticlesFromCharacter => true;
-
 }
