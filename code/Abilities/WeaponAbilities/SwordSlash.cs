2ad12f5fd6f3413b9913bf6b10f93b5508b32683
bakscratch <lt_8@hotmail.co.uk>
2022-06-11 20:26:28 +0100

    Added sword slash

--- code/Abilities/WeaponAbilities/SwordSlash.cs
+++ code/Abilities/WeaponAbilities/SwordSlash.cs
@@ -8,4 +8,7 @@ public partial class SwordSlash : BaseMeleeAttackAbility
 	public override string Description => "";
 	public override string Icon => "ui/ability_icons/sword_slash.png";
 	public override WeaponAbilityType Type => WeaponAbilityType.Attack;
+	public override string PreAbilityParticle => "particles/weapons/basic_sword/basic_swipe/basic_swipe.vpcf";
+	public override bool OriginateParticlesFromCharacter => true;
+
 }
