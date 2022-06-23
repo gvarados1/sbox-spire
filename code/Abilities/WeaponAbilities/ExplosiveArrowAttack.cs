6dbd2d0815ea0c6bd7db7497db943217feef16b8
Tony <devultj@gmail.com>
2022-06-23 23:46:43 +0100

    Explosive arrow will lodge into a player then explode after 0.5 seconds

--- code/Abilities/WeaponAbilities/ExplosiveArrowAttack.cs
+++ code/Abilities/WeaponAbilities/ExplosiveArrowAttack.cs
@@ -7,14 +7,23 @@ public partial class ExplosiveArrowAttack : BasicArrowAttack
 	public override string Identifier => "explosive_arrow_attack";
 	public override WeaponAbilityType Type => WeaponAbilityType.Ultimate;
 
-	protected override void OnProjectileHit( ProjectileEntity projectile, Entity hitEntity )
+	protected async Task DelayedExplosion( Entity hitEntity )
 	{
+		await GameTask.DelaySeconds( 0.5f );
+
 		new ExplosionEntity
 		{
-			Position = projectile.Position,
+			Position = hitEntity.Position,
 			Radius = Data.AbilityEffectRadius,
-			Damage = 20f,
+			Damage = 50f,
 			ForceScale = 1f,
-		}.Explode( projectile );
+		}.Explode( Owner );
+	}
+
+	protected override void OnProjectileHit( ProjectileEntity projectile, Entity hitEntity )
+	{
+		base.OnProjectileHit( projectile, hitEntity );
+
+		_ = DelayedExplosion( hitEntity );
 	}
 }
